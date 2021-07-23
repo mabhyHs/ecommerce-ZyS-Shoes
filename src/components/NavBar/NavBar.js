dcdb69317cd5cec1214625ffab53e8da8c35c399
Mabhy <m4huens@gmail.com>
2021-07-23 00:08:26 -0300

    menu-Huens

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css';
+
+const NavBar = () =>{
+    return(
+        <nav class="top-nav">
+            <div className="container">
+                <h1 className="brand-right">ZyS - Productos de Cuero</h1>
+                <p>Inicio</p>
+                <p>Productos</p>
+                <p>Contacto</p>
+            </div>
+       </nav>
+    );
+}
+
+export default NavBar;
